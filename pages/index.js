a268fa468ba86cb0f72f18444544aa142a4b8bc2
Lakshan Gunajothy <lakshangunajothy@Lakshans-MacBook-Pro.local>
2020-12-02 21:50:38 -0500

    changes made to markdowns and html file

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-export default [{"title":"About", "fname":"about.md", "specialImage":"/img/menagerie.png"},
+export default [{"title":"About", "fname":"about.md"},
 {"title":"index", "fname":"index.md"},
 {"title":"Contact", "fname":"contact.md"},
 {"title":"Portfolio", "fname":"portfolio.md"}];
