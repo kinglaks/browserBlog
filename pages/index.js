3d54a8382aca680bfe27c0b91ad529dde479e907
Lakshan Gunajothy <lakshangunajothy@Lakshans-MacBook-Pro.local>
2020-12-02 21:37:45 -0500

    making my markdown files work attempt 1

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
 export default [{"title":"About", "fname":"about.md", "specialImage":"/img/menagerie.png"},
 {"title":"index", "fname":"index.md"},
-{"title":"Contact", "fname":"contact.md"}];
+{"title":"Contact", "fname":"contact.md"},
+{"title":"Portfolio", "fname":"portfolio.md"}];
