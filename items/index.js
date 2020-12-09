ea6316df674f6ae2fc90e1d231a5494acd7785fc
Lakshan Gunajothy <lakshangunajothy@Lakshans-MacBook-Pro.local>
2020-12-09 12:29:18 -0500

    adding/removing images for the site

--- items/index.js
+++ items/index.js
@@ -1,3 +1,3 @@
-export default [{"id": "item1", "fname":"item1.md", "specialImage":"/img/FavouriteFoodBaskets750x500_xraj9x.png"},
-{"id": "item2", "fname":"item2.md", "specialImage":"/img/FavouriteDay750x500_pdni4p.png"},
-{"id": "item3", "fname":"item3.md", "specialImage":"/img/twiliobot750x500_z7anzp.png"}];
+export default [{"id": "item1", "fname":"item1.md", "specialImage":"/img/humanities.png"},
+{"id": "item2", "fname":"item2.md", "specialImage":"/img/makerLab1.png"},
+{"id": "item3", "fname":"item3.md", "specialImage":"/img/makerLab2.png"}];
